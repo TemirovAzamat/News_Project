321b294971562b962296d355db192cd9e4332cf7
Temirov Azamat <azamatt04@mail.ru>
2023-05-18 23:09:04 +0600

    fixing

--- news/serializers.py
+++ news/serializers.py
@@ -9,7 +9,7 @@ class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.News
         fields = '__all__'
-        read_only_fields = ['author']
+        read_only_fields = ['author', ]
 
 
 class CommentSerializer(serializers.ModelSerializer):
